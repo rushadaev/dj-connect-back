05cc3da7fb7cdc2e8418270a15fff24305a54d4b
beilec <rushadaev@gmail.com>
2024-07-10 01:55:35 -0700

    init

---
+++ dj-connect-back/app/Swagger/SwaggerAnnotations.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="DJ Connect API",
+ *      description="API documentation for DJ Connect",
+ *      @OA\Contact(
+ *          email="support@djconnect.com"
+ *      ),
+ *      @OA\License(
+ *          name="Apache 2.0",
+ *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *      )
+ * )
+ *
+ * @OA\Server(
+ *      url="http://localhost:8080/api/v1",
+ *      description="DJ Connect API Server"
+ * )
+ *
+ * @OA\SecurityScheme(
+ *      securityScheme="telegramAuth",
+ *      type="apiKey",
+ *      in="header",
+ *      name="Telegram-Init-Data"
+ * )
+ */
